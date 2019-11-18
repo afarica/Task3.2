58cbd4288e05220f0aad38db9d3fd279daf4c700
afarica <57631090+afarica@users.noreply.github.com>
2019-11-19 05:33:52 +0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,2 +1,4 @@
+# Попросить пользователя ввести слова через пробел. Отсортировать слова по количеству
+# символов и вывести на экран результат.
 source_text=list(input("Please,enter any text separated by spaces:").lower().split())
 print(sorted(source_text))
